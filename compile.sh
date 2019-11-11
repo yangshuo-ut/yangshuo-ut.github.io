0879e47741bedb22ba647a4a2323cb7e88bb9c8d
Shuo Yang <yangshuo_ut@utexas.edu>
2019-11-11 17:39:15 -0600

    update

--- compile.sh
+++ compile.sh
@@ -1,5 +1,12 @@
 #!bin/bash
 python jemdoc.py index
+
+# add google analytics script
+insert_line=8
+sed -e "${insert_line}r google_script.txt" index.html > temp.html
+cp temp.html index.html
+rm temp.html
+
 git add --a
 git commit -m"update"
 git push
